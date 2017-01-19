47119c87ace640fbd1242b18ec2c67239bf62521
Beth Nenniger <bethnenniger@gmail.com>
2017-01-18 22:24:07 -0500

    create a git repo for personal website with progress so far

---
+++ scripts.js
@@ -0,0 +1,3 @@
+function myConsole() {
+    console.log("Oh hey there! Don't panic -- I wrote this website from scratch using modern web technologies. Also, I am a designer, so my code is clean and functional. Get in touch with me now at hello@elizabethnenniger.com");
+}
